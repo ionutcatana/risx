b31d046e809d71105081e6740b1d545ece6af738
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-09-16 15:12:51 +0300

    small enhancements: - add panic message to panic handler - add doc comments

--- risx/src/lib.rs
+++ risx/src/lib.rs
@@ -1,12 +1,18 @@
 #![no_std]
 
+use crate::riscv::sbi::console_debug::write;
+
+/// This module includes RISC-V specific functionality.
 pub mod riscv;
 
 #[panic_handler]
+/// Print a panic message and halt the CPU.
 pub fn panic(_info: &core::panic::PanicInfo) -> ! {
+    let _ = write("panic!\n");
     loop {}
 }
 
+/// An idle loop that does nothing and never returns.
 pub fn idle_loop() -> ! {
     loop {}
 }
