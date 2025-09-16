aed6d072da60030a9591b53afcb7b5e64c66a441
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-09-16 15:13:55 +0300

    add appropriate types (moved from sbi/mod.rs)

--- risx/src/riscv/sbi/system_reset.rs
+++ risx/src/riscv/sbi/system_reset.rs
@@ -1,5 +1,16 @@
-// use super::SbiRet;
+use super::SbiRet;
 
-// unsafe extern "C" {
-//     pub fn sbi_system_reset(reset_type: u32, reset_reason: u32) -> SbiRet;
-// }
+unsafe extern "C" {
+    pub fn sbi_system_reset(reset_type: u32, reset_reason: u32) -> SbiRet;
+}
+
+pub enum ResetType {
+    Shutdown = 0,
+    ColdReboot = 1,
+    WarmReboot = 2,
+}
+
+pub enum ResetReason {
+    NoReason = 0,
+    SystemFailure = 1,
+}
