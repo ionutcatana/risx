4669350b303bdf88ace87ceb5461bc8c06a9148e
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-09-17 15:10:56 +0300

    simplify main.rs

--- risx/src/main.rs
+++ risx/src/main.rs
@@ -1,12 +1,9 @@
 #![no_std]
 #![no_main]
 
-use risx as _;
 use risx::idle_loop;
-use risx::riscv::sbi::util::extension_check;
 
 #[unsafe(no_mangle)]
-pub extern "C" fn main() -> ! {
-    extension_check();
+pub extern "C" fn main(_mhartid: isize, _dtb_ptr: isize) -> ! {
     idle_loop();
 }
