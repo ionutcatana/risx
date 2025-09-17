3dd2024b31e83f66174a51e1437c1ca007cef51e
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-09-17 15:10:45 +0300

    simplify entry.s

--- risx/src/riscv/entry.s
+++ risx/src/riscv/entry.s
@@ -1,19 +1,16 @@
-.section .rodata
-test_msg:
-        .ascii "Test message from risx!\n"
-
 .section .text.entry
 .globl _start
 
-.equ _stack_size, 0x100000              # 1MB stack
 _start:
-        li      a0, 24                  # number of bytes
-        la      a1, test_msg            # lower address
-        li      a2, 0                   # upper address
-        call    sbi_debug_console_write
-
+        # stack for the boot hart
         la      sp, _stack0
+        slli    t0, a0, 16
+        add     sp, sp, t0
+
+        # call main with args
+        # a0: hartid
+        # a1: dtb address
         call    main
 
-panic:
-        j       panic
+_halt:
+        j       _halt
